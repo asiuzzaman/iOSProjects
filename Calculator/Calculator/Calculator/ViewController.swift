cba2f1b00bfdca8c2d61e9669d9ad58e558edb07
asiuzzaman <zaman1304006@gmail.com>
2022-10-05 16:49:36 +0600

    Start change in viewController

--- Calculator/Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/Calculator/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var workingSpace: UILabel!
+    @IBOutlet weak var result: UILabel!
+    
+    var working: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        clearAll()
     }
-
-
+    
+    func clearAll() -> Void {
+        working = ""
+        workingSpace.text = ""
+        result.text = ""
+    }
+    
+    @IBAction func allClearTap(_ sender: Any) {
+        clearAll()
+    }
+    
+    @IBAction func backTap(_ sender: Any) {
+    }
+    
+    @IBAction func percentTap(_ sender: Any) {
+    }
+    
+    @IBAction func divideTap(_ sender: Any) {
+    }
+    @IBAction func multiplyTap(_ sender: Any) {
+    }
+    @IBAction func minusTap(_ sender: Any) {
+    }
+    @IBAction func plusTap(_ sender: Any) {
+    }
+    @IBAction func equalTap(_ sender: Any) {
+    }
+    
+    @IBAction func oneTap(_ sender: Any) {
+    }
+    
+    @IBAction func twoTap(_ sender: Any) {
+    }
+    
+    @IBAction func threeTap(_ sender: Any) {
+    }
+    @IBAction func fourTap(_ sender: Any) {
+    }
+    @IBAction func fiveTap(_ sender: Any) {
+    }
+    @IBAction func sixTap(_ sender: Any) {
+    }
+    @IBAction func sevenTap(_ sender: Any) {
+    }
+    @IBAction func eightTap(_ sender: Any) {
+    }
+    @IBAction func nineTap(_ sender: Any) {
+    }
+    
 }
 
